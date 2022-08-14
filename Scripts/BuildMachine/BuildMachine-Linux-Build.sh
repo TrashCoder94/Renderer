a297f47044490f3e195975d129b3eb027c6711e5
TrashCoder94 <jmottershead1994@gmail.com>
2022-08-14 15:40:28 +0100

    Update BuildMachine-Linux-Build.sh
    Recursive directory listings

--- Scripts/BuildMachine/BuildMachine-Linux-Build.sh
+++ Scripts/BuildMachine/BuildMachine-Linux-Build.sh
@@ -6,7 +6,7 @@ echo "Generating Project with Premake!"
 chmod +x ThirdParty/premake/Binaries/premake5
 ThirdParty/premake/Binaries/premake5 --file=premake5.lua gmake2
 
-ls -a
+ls -R
 
 make config=debug
 make config=release
