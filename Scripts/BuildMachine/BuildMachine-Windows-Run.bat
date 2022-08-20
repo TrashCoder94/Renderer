f92f8c3b331a5f5eda02ca10ed47c80ce80a2539
TrashCoder94 <jmottershead1994@gmail.com>
2022-08-20 17:52:49 +0100

    Update BuildMachine-Windows-Run.bat
    Attempting to pass API through batch file, means we should be able to run tests for DirectX12 and Vulkan on Windows.

--- Scripts/BuildMachine/BuildMachine-Windows-Run.bat
+++ Scripts/BuildMachine/BuildMachine-Windows-Run.bat
@@ -4,8 +4,16 @@ pushd %~dp0\..\..\
 set BinariesDirectory=Binaries\Debug-windows-x86_64\BuildMachineTest\
 
 cd %BinariesDirectory%
+echo "DirectX12"
+echo "========="
 echo "Running Debug Renderer BuildMachineTest executable!"
-call BuildMachineTest.exe
+call BuildMachineTest.exe -API DirectX12
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo "Vulkan"
+echo "======"
+echo "Running Debug Renderer BuildMachineTest executable!"
+call BuildMachineTest.exe -API Vulkan
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 popd
